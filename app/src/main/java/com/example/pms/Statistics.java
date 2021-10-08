3b902fd10a7dc1509599f1d4f9e3c86367459fc6
wnsdn2186 <wnsdn2186@naver.com>
2021-10-08 19:28:49 +0900

    #6 commit

--- app/src/main/java/com/example/pms/Statistics.java
+++ app/src/main/java/com/example/pms/Statistics.java
@@ -16,11 +16,6 @@ public class Statistics  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-
-        // toolbar setting
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,23 +23,5 @@ public class Statistics  extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.toolbar_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.Setting:
-                Toast.makeText(getApplicationContext(), "설정", Toast.LENGTH_LONG).show();
-                return true;
-
-            case R.id.Mypage:
-                Toast.makeText(getApplicationContext(), "마이페이지", Toast.LENGTH_LONG).show();
-                return true;
-
-            case R.id.Logout:
-                Toast.makeText(getApplicationContext(), "로그아웃", Toast.LENGTH_LONG).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
