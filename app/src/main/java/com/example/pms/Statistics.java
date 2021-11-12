e3d69ceb9ae3a7704d150b8a73976d9fe9dc5cc9
SeongSeokHwan <58363658+SeongSeokHwan@users.noreply.github.com>
2021-11-12 18:47:28 +0900

    commit
    commit

--- app/src/main/java/com/example/pms/Statistics.java
+++ app/src/main/java/com/example/pms/Statistics.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,139 +28,166 @@ import java.util.Collections;
 
 public class Statistics  extends AppCompatActivity {
     private Toolbar toolbar;
-    ArrayList<Integer> dayjsonList = new ArrayList<>(); // 출입차량 수
-    ArrayList<String> daylabelList = new ArrayList<>(); // 시간
-    ArrayList<Integer> timejsonList = new ArrayList<>(); // 출입차량 수
-    ArrayList<String> timelabelList = new ArrayList<>(); // 시간
-    BarChart daychart, timechart;
-    XAxis xAxisday, xAxistime;
-
+    ArrayList<Integer> timejsonList = new ArrayList<>(); //출입차량 수
+    ArrayList<String> timelabelList = new ArrayList<>(); //가로
+    ArrayList<Integer> dayjsonList = new ArrayList<>();
+    ArrayList<String> daylabelList = new ArrayList<>();
+    ArrayList<Integer> weekjsonList = new ArrayList<>(); //출입차량 수
+    ArrayList<String> weeklabelList = new ArrayList<>(); //가로
+    ArrayList<Integer> monthjsonList = new ArrayList<>();
+    ArrayList<String> monthlabelList = new ArrayList<>();
+    BarChart chart;
+    XAxis xAxis;
+    Button btnTime, btnDay, btnWeek, btnMonth;
+    ImageButton backbtn;
+    String value = "time";
+    BarDataSet depenses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
-        ImageButton backbtn = (ImageButton) findViewById(R.id.BackBtn);
-        backbtn.setOnClickListener(new View.OnClickListener() {
+        chart = (BarChart)findViewById(R.id.barchart);
+        xAxis = chart.getXAxis();
+        btnTime = (Button)findViewById(R.id.btn_time);
+        btnDay = (Button)findViewById(R.id.btn_day);
+        btnWeek = (Button)findViewById(R.id.btn_week);
+        btnMonth = (Button)findViewById(R.id.btn_month);
+        backbtn = (ImageButton) findViewById(R.id.BackBtn);
+
+        GraphInitSetting();
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                overridePendingTransition(R.anim.none, R.anim.horizon_exit);
+                switch (v.getId()) {
+                    case R.id.BackBtn:
+                        finish();
+                        break;
+                    case R.id.btn_time:
+                        value = "time";
+                        GraphInitSetting();
+                        break;
+                    case R.id.btn_day:
+                        value = "day";
+                        GraphInitSetting();
+                        break;
+                    case R.id.btn_week:
+                        value = "week";
+                        GraphInitSetting();
+                        break;
+                    case R.id.btn_month:
+                        value = "month";
+                        GraphInitSetting();
+                        break;
+                }
             }
-        });
-
-
-        daychart = (BarChart)findViewById(R.id.daybarchart);
-        timechart = (BarChart)findViewById(R.id.timebarchart);
-
-        xAxisday = daychart.getXAxis();
-        xAxistime = timechart.getXAxis();
-
-        GraphInitSetting(); // 그래프 세팅
-
-        DayBarChartGraph(daylabelList, dayjsonList);
-        TimeBarChartGraph(timelabelList, timejsonList);
+        };
+        btnTime.setOnClickListener(clickListener);
+        btnDay.setOnClickListener(clickListener);
+        btnWeek.setOnClickListener(clickListener);
+        btnMonth.setOnClickListener(clickListener);
     }
 
     public void GraphInitSetting(){
 
-        daylabelList.add("일");daylabelList.add("월");daylabelList.add("화");daylabelList.add("수");daylabelList.add("목");daylabelList.add("금");daylabelList.add("토"); // 요일
+        switch (value) {
+            case "time":
+                timelabelList.add("00시");timelabelList.add("01시");timelabelList.add("02시");timelabelList.add("03시");timelabelList.add("04시");timelabelList.add("05시");
+                timelabelList.add("06시");timelabelList.add("07시");timelabelList.add("08시");timelabelList.add("09시");timelabelList.add("10시");timelabelList.add("11시");
+                timelabelList.add("12시");timelabelList.add("13시");timelabelList.add("14시");timelabelList.add("15시");timelabelList.add("16시");timelabelList.add("17시");
+                timelabelList.add("18시");timelabelList.add("19시");timelabelList.add("20시");timelabelList.add("21시");timelabelList.add("22시");timelabelList.add("23시");
 
-        timelabelList.add("00시");timelabelList.add("01시");timelabelList.add("02시");timelabelList.add("03시");timelabelList.add("04시");timelabelList.add("05시");
-        timelabelList.add("06시");timelabelList.add("07시");timelabelList.add("08시");timelabelList.add("09시");timelabelList.add("10시");timelabelList.add("11시");
-        timelabelList.add("12시");timelabelList.add("13시");timelabelList.add("14시");timelabelList.add("15시");timelabelList.add("16시");timelabelList.add("17시");
-        timelabelList.add("18시");timelabelList.add("19시");timelabelList.add("20시");timelabelList.add("21시");timelabelList.add("22시");timelabelList.add("23시"); // 시간
+                timejsonList.add(10);timejsonList.add(10);timejsonList.add(50);timejsonList.add(30);timejsonList.add(20);timejsonList.add(44);
+                timejsonList.add(94);timejsonList.add(77);timejsonList.add(75);timejsonList.add(38);timejsonList.add(25);timejsonList.add(40);
+                timejsonList.add(2);timejsonList.add(63);timejsonList.add(59);timejsonList.add(32);timejsonList.add(63);timejsonList.add(80);
+                timejsonList.add(88);timejsonList.add(6);timejsonList.add(10);timejsonList.add(12);timejsonList.add(17);timejsonList.add(22);
 
-        // 임시값
-        dayjsonList.add(110);
-        dayjsonList.add(550);
-        dayjsonList.add(260);
-        dayjsonList.add(340);
-        dayjsonList.add(420);
-        dayjsonList.add(480);
-        dayjsonList.add(80);
+                chart.getAxisRight().setAxisMaxValue(100); // 최대값(add의 최대값 + 20정도로 하기)
+                chart.getAxisLeft().setAxisMaxValue(100);
+                chart.getAxisRight().setAxisMinValue(0); // 최대값(add의 최대값 + 20정도로 하기)
+                chart.getAxisLeft().setAxisMinValue(0);
 
-        // 임시값
-        timejsonList.add(10);timejsonList.add(10);timejsonList.add(50);timejsonList.add(30);timejsonList.add(20);timejsonList.add(44);
-        timejsonList.add(94);timejsonList.add(77);timejsonList.add(75);timejsonList.add(38);timejsonList.add(25);timejsonList.add(40);
-        timejsonList.add(2);timejsonList.add(63);timejsonList.add(59);timejsonList.add(32);timejsonList.add(63);timejsonList.add(80);
-        timejsonList.add(88);timejsonList.add(6);timejsonList.add(10);timejsonList.add(12);timejsonList.add(17);timejsonList.add(22);
+                chart.setTouchEnabled(false); // 터치불가
+                BarChartGraph(timelabelList, timejsonList);
+                break;
+            case "day":
+                daylabelList.add("일");daylabelList.add("월");daylabelList.add("화");daylabelList.add("수");daylabelList.add("목");daylabelList.add("금");daylabelList.add("토");
 
-        DayBarChartGraph(daylabelList, dayjsonList);
-        TimeBarChartGraph(timelabelList, timejsonList);
-        daychart.setTouchEnabled(false); // 터치불가 --> 추후 변경
+                dayjsonList.add(110);dayjsonList.add(550);dayjsonList.add(260);dayjsonList.add(340);dayjsonList.add(420);dayjsonList.add(480);dayjsonList.add(80);
+                chart.getAxisRight().setAxisMaxValue(700); // 최대값(add의 최대값 + 20정도로 하기)
+                chart.getAxisLeft().setAxisMaxValue(700);
+                chart.getAxisRight().setAxisMinValue(0); // 최대값(add의 최대값 + 20정도로 하기)
+                chart.getAxisLeft().setAxisMinValue(0);
 
-        daychart.getAxisRight().setAxisMaxValue(700); // 최대값(add의 최대값 + 20정도로 하기)
-        daychart.getAxisLeft().setAxisMaxValue(700);
-        daychart.getAxisRight().setAxisMinValue(0); // 최대값(add의 최대값 + 20정도로 하기)
-        daychart.getAxisLeft().setAxisMinValue(0);
+                chart.setTouchEnabled(false); // 터치불가
+                BarChartGraph(daylabelList, dayjsonList);
+                break;
+            case "week":
 
-        timechart.getAxisRight().setAxisMaxValue(100); // 최대값(add의 최대값 + 20정도로 하기)
-        timechart.getAxisLeft().setAxisMaxValue(100);
-        timechart.getAxisRight().setAxisMinValue(0); // 최대값(add의 최대값 + 20정도로 하기)
-        timechart.getAxisLeft().setAxisMinValue(0);
+                break;
+            case "month":
+
+                break;
+        }
     }
 
-    private void DayBarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
+    private void BarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
 
         ArrayList<BarEntry> entries = new ArrayList<>(); // 출입차량 넣기
         for (int i = 0; i < valList.size(); i++) {
             entries.add(new BarEntry((Integer) valList.get(i), i));
         }
 
-        BarDataSet depenses = new BarDataSet(entries, "일간 출입차량"); // 이름
-        depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
-        daychart.setDescription(" ");
+        switch (value) {
+            case "time":
+                depenses = new BarDataSet(entries, "시간별 출입차량");
+                depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+                chart.setDescription(" ");
 
-        ArrayList<String> labels = new ArrayList<String>(); //요일 한글표시
+                depenses.setColors(Collections.singletonList(Color.rgb(77, 148, 255)));
+                chart.setVisibleXRangeMaximum(24f);
+                break;
+            case "day":
+                depenses = new BarDataSet(entries, "일간 출입차량"); // 이름
+                depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+                chart.setDescription(" ");
+
+                depenses.setColors(Collections.singletonList(Color.rgb(0, 102, 255))); // 컬러 팔레트
+                chart.setVisibleXRangeMaximum(7f); // 갯수표시
+                break;
+            case "week":
+                depenses = new BarDataSet(entries, "주간 출입차량"); // 이름
+                depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+                chart.setDescription(" ");
+
+                depenses.setColors(Collections.singletonList(Color.rgb(0, 102, 255))); // 컬러 팔레트
+                chart.setVisibleXRangeMaximum(5f); // 갯수표시
+                break;
+            case "month":
+                depenses = new BarDataSet(entries, "월간 출입차량"); // 이름
+                depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
+                chart.setDescription(" ");
+
+                depenses.setColors(Collections.singletonList(Color.rgb(0, 102, 255))); // 컬러 팔레트
+                chart.setVisibleXRangeMaximum(12f); // 갯수표시
+                break;
+        }
+
+        ArrayList<String> labels = new ArrayList<String>(); //한글표시
         for (int i = 0; i < labelList.size(); i++) {
             labels.add((String) labelList.get(i));
         }
 
-        //그래프 디자인
-        depenses.setColors(Collections.singletonList(Color.rgb(0, 102, 255))); // 컬러 팔레트
-        daychart.setVisibleXRangeMaximum(7f); // 갯수표시
-        xAxisday.setPosition(XAxis.XAxisPosition.BOTTOM); // 요일 표시 하단
-        xAxisday.setDrawGridLines(false); // 세로선 삭제
-        //barChart.getAxisLeft().setEnabled(false); // 왼쪽 수치 표시x
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // 표시 하단
+        xAxis.setDrawGridLines(false); // 세로선 삭제
 
         BarData data = new BarData(labels, depenses);
         data.setValueFormatter(new MyValueFormatter());
 
-        daychart.setData(data);
-        daychart.animateXY(1000, 1000);
-        daychart.invalidate();
-    }
-
-    private void TimeBarChartGraph(ArrayList<String> labelList, ArrayList<Integer> valList) {
-
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        for (int i = 0; i < valList.size(); i++) {
-            entries.add(new BarEntry((Integer) valList.get(i), i));
-        }
-
-        BarDataSet depenses = new BarDataSet(entries, "시간별 출입차량");
-        depenses.setAxisDependency(YAxis.AxisDependency.LEFT);
-        timechart.setDescription(" ");
-
-        ArrayList<String> labels = new ArrayList<String>();
-        for (int i = 0; i < labelList.size(); i++) {
-            labels.add((String) labelList.get(i));
-        }
-
-        //그래프 디자인
-        depenses.setColors(Collections.singletonList(Color.rgb(77, 148, 255)));
-        timechart.setVisibleXRangeMaximum(24f);
-        xAxistime.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxistime.setDrawGridLines(false);
-        //timechart.getAxisLeft().setEnabled(false);
-
-        BarData data = new BarData(labels, depenses);
-        data.setValueFormatter(new MyValueFormatter());
-
-        timechart.setData(data);
-        timechart.animateXY(1000, 1000);
-        timechart.invalidate();
+        chart.setData(data);
+        chart.animateXY(1000, 1000);
+        chart.invalidate();
     }
 
     public class MyValueFormatter implements ValueFormatter {
@@ -188,8 +216,6 @@ public class Statistics  extends AppCompatActivity {
 2. 컬러 변경 - 단색에서 다색으로
 3. 일간, 시간 데이터 현재를 제일 우측 표시
 4. 현재 기준으로 필요 수 만큼 가져오기
-5. 소수점 제거
-6. 터치 기능 수정
  + 디자인
 */
 
