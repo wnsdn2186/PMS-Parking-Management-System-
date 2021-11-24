94d7f0b52b3fdb6f816fa84892ac8c5689deb278
Seungwoo Son <jsmith032211@gmail.com>
2021-11-25 00:15:36 +0900

    commit
    - passtime

--- app/src/main/java/com/example/pms/Statistics.java
+++ app/src/main/java/com/example/pms/Statistics.java
@@ -1,11 +1,15 @@
 package com.example.pms;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -21,12 +25,24 @@ import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Statistics  extends AppCompatActivity {
     private Toolbar toolbar;
+    private static String IP_ADDRESS = "13.59.85.177";
+    private String mJsonString;
     ArrayList<Integer> dayjsonList = new ArrayList<>(); // 출입차량 수
     ArrayList<String> daylabelList = new ArrayList<>(); // 시간
     ArrayList<Integer> timejsonList = new ArrayList<>(); // 출입차량 수
@@ -48,6 +64,8 @@ public class Statistics  extends AppCompatActivity {
             }
         });
 
+        GetData task = new GetData();
+        task.execute("http://" + IP_ADDRESS + "/stat.php", "");
 
         daychart = (BarChart)findViewById(R.id.daybarchart);
         timechart = (BarChart)findViewById(R.id.timebarchart);
@@ -181,6 +199,105 @@ public class Statistics  extends AppCompatActivity {
         menuInflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(Statistics.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+
+            Log.d("response", "response - " + result);
+
+            if (result == null){
+                //rs.setText(errorString);
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = params[0];
+            String postParameters = params[1];
+
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("response", "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                Log.d("error", "GetData : Error ", e);
+                errorString = e.toString();
+                return null;
+            }
+
+        }
+    }
+
+    private void showResult(){
+        String TAG_JSON="statData";
+        String TAG_PASSTIME = "passtime";
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            for(int i=0;i<jsonArray.length();i++){
+                JSONObject item = jsonArray.getJSONObject(i);
+                int pt = item.getInt(TAG_PASSTIME);
+                Log.i("passtime: ", String.valueOf(pt));
+            }
+        } catch (JSONException e) {
+            Log.d("result", "showResult : ", e);
+        }
+    }
+
 }
 
 /*
